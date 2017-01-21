76ff68b10768566eebea878016bb51c0922ba7cc
Alberto González Tajuelo <albertajuelo@gmail.com>
2017-01-21 15:54:30 +0100

    Change path to dest on lineinfile module

--- files/create_admin.sh
+++ files/create_admin.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo -e "$1\n$1" | kadmin.local  -q "addprinc $2/admin"
-
+echo -e "$1\n$1" | kadmin.local -q "addprinc $2/admin"
--- files/create_db.sh
+++ files/create_db.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 echo -e "$1\n$1" | kdb5_util create -s
-
